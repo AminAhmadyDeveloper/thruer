1da348f6dffc533750b332b3896d406d8fe1a36d
Amin Ahmady <aminahmadydeveloper@gmail.com>
2025-09-15 00:49:27 -0700

    feat: add dialog, drawer and credenza components with font utilities

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { getLocale } from "next-intl/server";
 import type { PropsWithChildren } from "react";
+import { mono, sans } from "@/lib/font-utils";
 import { cn } from "@/lib/tailwind-utils";
 import { Providers } from "@/providers";
 
@@ -8,7 +9,10 @@ const RootLayout: NextLayout<PropsWithChildren> = async ({ children }) => {
 
   return (
     <html suppressContentEditableWarning suppressHydrationWarning lang={l}>
-      <body className={cn("antialiased")}>
+      <body
+        className={cn(sans.variable, mono.variable, "antialiased")}
+        vaul-drawer-wrapper=""
+      >
         <Providers>{children}</Providers>
       </body>
     </html>
